c5626e2f35c6beafc246fec3df8d62235968a0be
Kamesh <Kamesh@Kamesh-PC.Dlink>
2018-09-09 00:05:37 +0530

    added @author

--- EstiloxBusinessServices/src/main/java/com/estilox/application/load/ApplicationStartup.java
+++ EstiloxBusinessServices/src/main/java/com/estilox/application/load/ApplicationStartup.java
@@ -3,9 +3,11 @@ package com.estilox.application.load;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * @author Kamesh
+ **/
 @SpringBootApplication
-public class ApplicationStartup {
+public class ApplicationStartup{
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationStartup.class, args);
 	}
